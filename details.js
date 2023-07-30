3335e5335d1b4a4371f9b1cf646b5371963a67ce
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-07-30 19:59:33 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '6329058372:AAEojH3iTeOdOfFOFjGGnP3Vc846KKh-bLc',
+    token: '5911131844:AAF_FezL34ks4_f0HfpsVeRuNGzGtbaF6Rs',
     admins: [5556717138],
     mongo_url: 'mongodb+srv://manishprajapati7517:R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority',
 }
